a4558cd68c8922750b4e3d5a431f7e33ee9c5d4a
Clark Roman <clarkroma4@gmail.com>
2019-12-10 13:35:28 -0600

    Added IntroductionCodeAlong

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The area of the rectangle is " + height * width);
+        }
+    }
+}
